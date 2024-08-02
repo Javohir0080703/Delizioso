320ce03e55372742de989732d779b887f48a61f6
Javohir0080703 <renobobopocta@gmail.com>
2024-08-02 15:31:09 +0500

    add to Home_Reserve_Table component

---
+++ src/components/Home_Reserve_Table.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Home_reserve_table_img from '../assets/img/Home_reserve_table_img.png'
+const Home_Reserve_Table = () => {
+  return (
+    <div>Home_Reserve_Table</div>
+  )
+}
+
+export default Home_Reserve_Table
